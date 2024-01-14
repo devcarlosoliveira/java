1bcf60ad4dc56a3f38b369d9d98b43960216d9c8
Carlos Oliveira <68062585+devcarlosoliveira@users.noreply.github>
2024-01-14 14:40:53 +0000

    correção da mensagem de validação do campo password

--- gestao_vagas/src/main/java/br/com/carlos_oliveira/gestao_vagas/modules/candidate/model/CandidateEntity.java
+++ gestao_vagas/src/main/java/br/com/carlos_oliveira/gestao_vagas/modules/candidate/model/CandidateEntity.java
@@ -21,7 +21,7 @@ public class CandidateEntity {
 	@Email(message = "O campo [email] deve conter um e-mail válido")
 	private String email;
 
-	@Length(min = 10, max = 100)
+	@Length(min = 10, max = 100, message = "A senha deve conter entre (10) e (100) caracteres")
 	private String password;
 
 	private String description;
