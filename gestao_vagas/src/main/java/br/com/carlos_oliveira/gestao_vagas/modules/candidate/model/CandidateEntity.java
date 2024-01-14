193e34d86f4b056837602dd7594f40fc52bf2046
Carlos Oliveira <68062585+devcarlosoliveira@users.noreply.github>
2024-01-14 16:15:25 +0000

    criando tabela de candidato

--- gestao_vagas/src/main/java/br/com/carlos_oliveira/gestao_vagas/modules/candidate/model/CandidateEntity.java
+++ gestao_vagas/src/main/java/br/com/carlos_oliveira/gestao_vagas/modules/candidate/model/CandidateEntity.java
@@ -1,16 +1,26 @@
 package br.com.carlos_oliveira.gestao_vagas.modules.candidate.model;
 
+import java.time.LocalDateTime;
 import java.util.UUID;
 
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
 import org.hibernate.validator.constraints.Length;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.Pattern;
 import lombok.Data;
 
 @Data
+@Entity(name = "candidate")
 public class CandidateEntity {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.UUID)
 	private UUID id;
 
 	private String name;
@@ -27,5 +37,11 @@ public class CandidateEntity {
 	private String description;
 
 	private String curriculum;
+
+	@CreationTimestamp
+	private LocalDateTime createAt;
+
+	@UpdateTimestamp
+	private LocalDateTime updateAt;
 
 }
